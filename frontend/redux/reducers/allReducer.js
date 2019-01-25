41998583ccf2d02d99545c896af783ca5ed8289c
VitaliiKalinbet <bosssvit@gmail.com>
2019-01-25 12:24:28 +0200

    before npm run eject

---
+++ frontend/redux/reducers/allReducer.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux'; // объединяет все поля в один объект
+import users from './usersReducer';
+import currentUser from './currentUserReducer';
+import channels from './channelReducer';
+
+const rootReducer = combineReducers({
+    users: users,
+    currentUser: currentUser,
+    channels: channels,
+})
+
+export default rootReducer;
