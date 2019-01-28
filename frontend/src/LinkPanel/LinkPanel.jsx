4e28a874410d14ffa2e0e95afc343b815d10b7be
Alexandr Zhuk <sanek201dg@ukr.net>
2019-01-28 15:13:00 +0200

    two second

--- frontend/src/LinkPanel/LinkPanel.jsx
+++ frontend/src/LinkPanel/LinkPanel.jsx
@@ -6,24 +6,27 @@ import linkPanel_calendar from '../img/linkPanel_calendar.png'
 import linkPanel_icons from '../img/linkPanel_icon.png'
 import linkPanel_change from '../img/linkPanel_change.png'
 import linkPanel_add from '../img/linkPanel_add.png'
+import { FaGooglePlus , FaLinkedin } from 'react-icons/fa';
 
 class LinkPanel extends Component {
-  // state = {
-  //   iconPack:[
-  //     {
-  //       urlGoogle: <i class="fab fa-google-plus"></i>,
-  //     },
-  //     {
-  //       urlLinkedin:
-  //     }
-  //   ]
-  // }
+  state = {
+    iconPack:[
+      {
+        urlGoogle: <FaGooglePlus/>,
+      },
+      {
+        urlLinkedin: <FaLinkedin/>
+      }
+    ]
+  }
 
   render() {
     return (
       <div className="linkPanel_container">
         <div>
-          {/* <div>{this.state.iconPack[0].urlGoogle}</div> */}
+          <ul>
+            <li>{this.state.iconPack[0].urlGoogle}</li>
+          </ul>
           <img className="link-panel_search" src={linkPanel_search} alt="search"/>
           <img className="link-panel_line" src={linkPanel_figure} alt="figure"/>
           <img className="link-panel_calendar" src={linkPanel_calendar} alt="calendar"/>
