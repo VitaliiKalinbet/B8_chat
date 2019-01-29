df7e078b8048f0faf1cedd6814113a3726240fac
VitaliiKalinbet <bosssvit@gmail.com>
2019-01-29 20:41:33 +0200

    change in chat component

--- frontend/src/LinkPanel/LinkPanel.jsx
+++ frontend/src/LinkPanel/LinkPanel.jsx
@@ -7,62 +7,62 @@ import linkPanel_calendar from '../img/linkPanel_calendar.png'
 import linkPanel_icons from '../img/linkPanel_icon.png'
 import linkPanel_change from '../img/linkPanel_change.png'
 import linkPanel_add from '../img/linkPanel_add.png'
-import { FaGooglePlus , FaLinkedinIn, FaFacebookSquare, FaTrello, FaGoogleDrive, FaCalendarAlt, FaEnvelope, FaMusic, FaFileImage } from 'react-icons/fa';
+import { FaGooglePlus, FaLinkedinIn, FaFacebookSquare, FaTrello, FaGoogleDrive, FaCalendarAlt, FaEnvelope, FaMusic, FaFileImage } from 'react-icons/fa';
 import { MdStar } from "react-icons/md";
 import { IconContext } from "react-icons";
 
 class LinkPanel extends Component {
   state = {
-    showModal:false,
+    showModal: false,
     imgActive: '',
     links: [
       {
         linkName: 'Google search',//информация которую вводит юзер
-        url:'https://www.google.com/webhp',//информация которую вводит юзер
-        iconName:'./images/googleIcon.png',//иконка которую добавляет юзер
+        url: 'https://www.google.com/webhp',//информация которую вводит юзер
+        iconName: './images/googleIcon.png',//иконка которую добавляет юзер
       }
     ],
     modalInputName: '',
     modalInputLink: '',
-    iconPack:[
+    iconPack: [
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaGooglePlus/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaGooglePlus /></IconContext.Provider>,
         id: 1,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaLinkedinIn/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaLinkedinIn /></IconContext.Provider>,
         id: 2,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaFacebookSquare/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaFacebookSquare /></IconContext.Provider>,
         id: 3,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaTrello/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaTrello /></IconContext.Provider>,
         id: 4,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaGoogleDrive/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaGoogleDrive /></IconContext.Provider>,
         id: 5,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaCalendarAlt/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaCalendarAlt /></IconContext.Provider>,
         id: 6,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaEnvelope/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaEnvelope /></IconContext.Provider>,
         id: 7,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><MdStar/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><MdStar /></IconContext.Provider>,
         id: 8,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaMusic/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaMusic /></IconContext.Provider>,
         id: 9,
       },
       {
-        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaFileImage/></IconContext.Provider>,
+        url: <IconContext.Provider value={{ size: '1.5em', className: `${style.global}` }}><FaFileImage /></IconContext.Provider>,
         id: 10,
       }
     ]
@@ -76,9 +76,9 @@ class LinkPanel extends Component {
     }))
   }
 
-  handleChange =({target}) => {
+  handleChange = ({ target }) => {
     this.setState({
-        [target.name] : target.value,
+      [target.name]: target.value,
     });
   };
 
@@ -89,35 +89,32 @@ class LinkPanel extends Component {
   }
 
   render() {
-    const {showModal, modalInputName, modalInputLink, imgActive} = this.state
+    const { showModal, modalInputName, modalInputLink, imgActive } = this.state
     return (
       <div className={style.linkPanel_container}>
         <div className={style.link_height}>
-          {/* <ul>
-            <li>{this.state.iconPack[0].urlGoogle}</li>
-          </ul> */}
           <div className={style.icons}>
-          <img className={style.panel_icons} src={linkPanel_search} alt="search"/>
-          <img className={style.panel_line} src={linkPanel_figure} alt="figure"/>
-          <img className={style.panel_icons} src={linkPanel_calendar} alt="calendar"/>
-          <img className={style.panel_line} src={linkPanel_figure} alt="figure"/>
-          <img className={style.panel_icons} src={linkPanel_icons} alt="icons"/>
-          <img className={style.panel_line} src={linkPanel_figure} alt="figure"/>
-          <img className={style.panel_icons} src={linkPanel_change} alt="change"/>
-          <img className={style.panel_line} src={linkPanel_figure} alt="figure"/>
+            <img className={style.panel_icons} src={linkPanel_search} alt="search" />
+            <img className={style.panel_line} src={linkPanel_figure} alt="figure" />
+            <img className={style.panel_icons} src={linkPanel_calendar} alt="calendar" />
+            <img className={style.panel_line} src={linkPanel_figure} alt="figure" />
+            <img className={style.panel_icons} src={linkPanel_icons} alt="icons" />
+            <img className={style.panel_line} src={linkPanel_figure} alt="figure" />
+            <img className={style.panel_icons} src={linkPanel_change} alt="change" />
+            <img className={style.panel_line} src={linkPanel_figure} alt="figure" />
           </div>
           <div className={style.panel_add} onClick={this.toggleModal}>
-          <img src={linkPanel_add} alt="add"/>
+            <img src={linkPanel_add} alt="add" className={style.button}/>
           </div>
         </div>
         {showModal && <Modal toggleModal={this.toggleModal} name={'Add new channel'} imgActive={imgActive} modalInputLink={modalInputLink} modalInputName={modalInputName}>
-        <input className={style.modalInput} value={modalInputName}  onChange={this.handleChange} type="text" name='modalInputName' placeholder='Enter a icon name'/>
-        <input className={style.modalInput} value={modalInputLink} onChange={this.handleChange} type="text" name='modalInputLink' placeholder='Enter a icon link'/>
-        <h4>Choose the icon:</h4>
-        <ul className={style.card}>
-          {this.state.iconPack.map(el => <li onClick={() => this.activeSvg(el.id)} key={el.id} className={imgActive === el.id? `${style.svgLink}` : `${style.svgFalse}`}>{el.url}</li>)}
-        </ul>
-      </Modal>}
+          <input className={style.modalInput} value={modalInputName} onChange={this.handleChange} type="text" name='modalInputName' placeholder='Enter a icon name' />
+          <input className={style.modalInput} value={modalInputLink} onChange={this.handleChange} type="text" name='modalInputLink' placeholder='Enter a icon link' />
+          <h4>Choose the icon:</h4>
+          <ul className={style.card}>
+            {this.state.iconPack.map(el => <li onClick={() => this.activeSvg(el.id)} key={el.id} className={imgActive === el.id ? `${style.svgLink}` : `${style.svgFalse}`}>{el.url}</li>)}
+          </ul>
+        </Modal>}
       </div>
     );
   }
--- frontend/src/userPanel/userPanel.jsx
+++ frontend/src/userPanel/userPanel.jsx
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import style from './userPanel.module.css';
 import Modal from '../sidePanelModal/modal';
 import AvatarEditor from 'react-avatar-editor';
-
+import { FaEllipsisH } from 'react-icons/fa';
 
 export default class UserPanel extends Component {
+
     state = {
         dropdown: false,
         showModal: false,
-        previewImage:'',
-        croppedImage:'',
-        blob:'',
+        previewImage: '',
+        croppedImage: '',
+        blob: '',
     }
 
     toggleDropdown = () => {
@@ -28,16 +29,16 @@ export default class UserPanel extends Component {
     handleChange = event => {
         const file = event.target.files[0];
         const reader = new FileReader();
-        if(file) {
+        if (file) {
             reader.readAsDataURL(file);
             reader.addEventListener('load', () => {
-                this.setState({ previewImage: reader.result});
+                this.setState({ previewImage: reader.result });
             })
         }
     };
 
     handleCropImage = () => {
-        if(this.avatarEditor) {
+        if (this.avatarEditor) {
             this.avatarEditor.getImageScaledToCanvas().toBlob(blob => {
                 let imageUrl = URL.createObjectURL(blob);
                 this.setState({
@@ -48,38 +49,40 @@ export default class UserPanel extends Component {
         }
     };
 
-  render() {
-    const imageSrc = 'https://cdn.vox-cdn.com/thumbor/TiwabydzgLgAVBjjJvAO_dnKo_o=/0x16:1103x751/1200x800/filters:focal(0x16:1103x751)/cdn.vox-cdn.com/uploads/chorus_image/image/46840054/Screenshot_2015-07-27_15.11.13.0.0.png';
-     let {dropdown, showModal, previewImage, croppedImage} = this.state;
-    return (
-      <div className={style.wrapper}>
-        <img className={style.avatar} src={imageSrc} alt='avatar'/>
-        <span className={style.userName}>Valentine</span>
-        <div className={style.dropdownWrapper}>
-            <button className={style.dropdownBtn} onClick={this.toggleDropdown}><i className="fa fa-ellipsis-h" style={{fontSize:'1rem', color:'#7b8aa4'}}></i></button>
-            {dropdown && <div className={style.dropdownList} onClick={this.toggleDropdown}>
-                <span><i className="fa fa-sign-out"></i> Sign Out</span>
-                <span onClick={this.toggleModal}><i className="fa fa-image"></i> Change Avatar</span>
-            </div>}
-        </div>
-        {showModal && <Modal toggleModal={this.toggleModal} name={'Change avatar'} >
-            <input className={style.btn}type="file" onChange={this.handleChange}/>
-            {previewImage && <div className={style.previewBloack}>
-            {previewImage && this.handleCropImage()}
-                <AvatarEditor
-                    onLoadSuccess={this.handleCropImage}
-                    ref={node => (this.avatarEditor = node)}
-                    image={previewImage}
-                    width={120}
-                    height={120}
-                    border={50}
-                    scale={1.2}/>
-                <span><i><i style={{fontSize:'3rem', color:'#273247'}} class="fa fa-arrow-circle-o-right"></i></i></span>
-                {croppedImage  && <img className={style.prevImg} src={croppedImage} alt='prev img'/>}
-            </div>}
-        </Modal>}
-      </div>
-    )
-  }
+    render() {
+        const imageSrc = 'https://cdn.vox-cdn.com/thumbor/TiwabydzgLgAVBjjJvAO_dnKo_o=/0x16:1103x751/1200x800/filters:focal(0x16:1103x751)/cdn.vox-cdn.com/uploads/chorus_image/image/46840054/Screenshot_2015-07-27_15.11.13.0.0.png';
+        let { dropdown, showModal, previewImage, croppedImage } = this.state;
+        return (
+            <div className={style.wrapper}>
+                <div className={style.avatarAndUsername}>
+                    <img className={style.avatar} src={imageSrc} alt='avatar' />
+                    <span className={style.userName}>Valentine</span>
+                </div>
+                <div className={style.dropdownWrapper}>
+                    <FaEllipsisH onClick={this.toggleDropdown} className={style.dropdownBtn}/>
+                    {dropdown && <div className={style.dropdownList} onClick={this.toggleDropdown}>
+                        <span><i className="fa fa-sign-out"></i> Sign Out</span>
+                        <span onClick={this.toggleModal}><i className="fa fa-image"></i> Change Avatar</span>
+                    </div>}
+                </div>
+                {showModal && <Modal toggleModal={this.toggleModal} name={'Change avatar'} >
+                    <input className={style.btn} type="file" onChange={this.handleChange} />
+                    {previewImage && <div className={style.previewBloack}>
+                        {previewImage && this.handleCropImage()}
+                        <AvatarEditor
+                            onLoadSuccess={this.handleCropImage}
+                            ref={node => (this.avatarEditor = node)}
+                            image={previewImage}
+                            width={120}
+                            height={120}
+                            border={50}
+                            scale={1.2} />
+                        <span><i><i style={{ fontSize: '3rem', color: '#273247' }} class="fa fa-arrow-circle-o-right"></i></i></span>
+                        {croppedImage && <img className={style.prevImg} src={croppedImage} alt='prev img' />}
+                    </div>}
+                </Modal>}
+            </div>
+        )
+    }
 }
 
