05cb2f6dc12afb628b31fe259b9426f4b5b3a3d0
VitaliiKalinbet <bosssvit@gmail.com>
2019-03-13 00:03:56 +0200

    make build in frontend

---
+++ frontend/build/precache-manifest.8403bf85f6418762cae901c7ab7c4c43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2072db6fa552139858705d47193d53",
+    "url": "/static/media/registration.ab2072db.jpg"
+  },
+  {
+    "revision": "a74d3c17ffd4b6b1dde5",
+    "url": "/static/css/main.9b420daa.chunk.css"
+  },
+  {
+    "revision": "1a48d8b18940752eeb8897f1ba710c87",
+    "url": "/static/media/delete_blue.1a48d8b1.svg"
+  },
+  {
+    "revision": "ff1b35e652a0dce5fdd4",
+    "url": "/static/js/1.ff1b35e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a74d3c17ffd4b6b1dde5",
+    "url": "/static/js/main.a74d3c17.chunk.js"
+  },
+  {
+    "revision": "f999ac00bc9abb63c8738da2d37301d8",
+    "url": "/static/media/cutLogin.f999ac00.jpg"
+  },
+  {
+    "revision": "554bf8f48b0a6f5b68227dabd0439c75",
+    "url": "/static/media/edit.554bf8f4.svg"
+  },
+  {
+    "revision": "2d47abecd01d5a0d464ed3830bf92b07",
+    "url": "/static/media/delete.2d47abec.svg"
+  },
+  {
+    "revision": "ff1b35e652a0dce5fdd4",
+    "url": "/static/css/1.22964db2.chunk.css"
+  },
+  {
+    "revision": "bc0ee254b6aaa25c35ea5cfcc8203835",
+    "url": "/index.html"
+  }
+];
